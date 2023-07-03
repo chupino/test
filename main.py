4c868bb91b440ce2f253471f0ff94809f01fe018
Mauricio <mauriciodiego2019@gmail.com>
2023-07-03 14:58:04 -0500

    modulos

--- main.py
+++ main.py
@@ -1,55 +1,6 @@
-def agregar_tarea():
-    tarea = input("Ingrese la nueva tarea: ")
-    tareas.append(tarea)
-    print("Tarea agregada con éxito.")
-
-def marcar_completada():
-    if len(tareas) == 0:
-        print("No hay tareas pendientes.")
-    else:
-        print("Tareas pendientes:")
-        for i, tarea in enumerate(tareas):
-            print(f"|   {i+1}            | {tarea:<15}|")
-
-        num_tarea = int(input("Ingrese el número de la tarea completada: "))
-        if num_tarea < 1 or num_tarea > len(tareas):
-            print("Número de tarea inválido.")
-        else:
-            tarea_completada = tareas.pop(num_tarea - 1)
-            tareas_completadas.append(tarea_completada)
-            print(f"La tarea '{tarea_completada}' ha sido marcada como completada.")
-
-def mostrar_pendientes():
-    if len(tareas) == 0:
-        print("No hay tareas pendientes.")
-    else:
-        print(
-          "]"
-        )
-        print("Tareas pendientes:")
-        print("+------+-----------------+")
-        print("| Número |      Tarea    |")
-        print("+------+-----------------+")
-        for i, tarea in enumerate(tareas):
-            print(f"|   {i+1}   | {tarea:<15}|")
-        print("+------+-----------------+")
+from functions.functions import *
 
 
-def mostrar_ultima_completada():
-    if len(tareas_completadas) == 0:
-        print("No hay tareas completadas.")
-    else:
-        tarea_completada = tareas_completadas[-1]
-        print("Última tarea completada:")
-        print("+------+-----------------+")
-        print("| Número |      Tarea      |")
-        print("+------+-----------------+")
-        print(f"|   1   | {tarea_completada:<15}|")
-        print("+------+-----------------+")
-
-
-tareas = []
-tareas_completadas = []
 
 
 while True:
